1b766cbcf086cccebeb3bd1ba4e2e37ba9c55686
yohanes <yohanessitorusp@gmail.com>
2024-12-25 12:23:23 +0700

    [FEAT] tugas 1

--- client/client.go
+++ client/client.go
@@ -1,19 +1,45 @@
 package main
 
 import (
-	// "bufio"
+	"bufio"
 	"fmt"
 	"net"
 	"os"
 )
 
 func main() {
+	// Menghubungkan client ke server
 	conn, err := net.Dial("tcp", ":9090")
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Cannot connect to server!")
-    } else {
-        fmt.Println("Connected to server!")
-    }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error connecting to server: %v\n", err)
+	} else {
+		fmt.Println("Connected to server. Type your message and press Enter to send.")
+	}
 
-	fmt.Print(conn)
+	defer conn.Close()
+
+	// Goroutine untuk mendengarkan pesan dari server
+	go func() {
+		connReader := bufio.NewReader(conn)
+		for {
+			_, err := connReader.ReadString('\n')
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Cannot read the message: %v\n", err)
+				return
+			} else {
+				fmt.Println("The message has been read!")
+			}
+		}
+	}()
+
+	// Loop untuk membaca input pengguna dan mengirimkannya ke server
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		message := scanner.Text()
+		if message == "exit" {
+			fmt.Println("Exiting chat...")
+			break
+		}
+		conn.Write([]byte(message + "\n"))
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,27 +1,83 @@
 package main
 
 import (
-	// "bufio"
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
+	"sync"
+)
+
+var (
+	clients = make(map[net.Conn]string) // Menyimpan semua koneksi client
+	mtx     sync.Mutex                  // Mutex untuk akses aman ke map clients
 )
 
 func main() {
+	// Membuat server untuk mendengarkan di port 9000
 	ln, err := net.Listen("tcp", ":9090")
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to listen!")
-        os.Exit(1)
-    } else {
-        fmt.Println("Listening...")
-    }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to listen: %v\n", err)
+		os.Exit(1)
+	} else {
+		fmt.Println("Server is listening on port 9090...")
+	}
+	defer ln.Close()
 
-    fmt.Println(ln)
-    // conn, err := ln.Accept()
-    // if err != nil {
-    //     fmt.Fprintf(os.Stderr, "Failed to accept connection!")
-    //     os.Exit(1)
-    // } else {
-    //     fmt.Println("New connection accepted!")
-    // }
+	for {
+		// Menerima koneksi baru
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error accepting connection: %v\n", err)
+			continue
+		}
+
+		// Menangani koneksi client dalam goroutine
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	// Menambahkan client ke daftar
+	mtx.Lock()
+	clients[conn] = conn.RemoteAddr().String()
+	mtx.Unlock()
+
+	fmt.Printf("Client %s connected.\n", conn.RemoteAddr())
+
+	reader := bufio.NewReader(conn)
+	fmt.Println("Waiting for message...")
+	for {
+		// Membaca pesan dari client
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to read message: %v\n", err)
+			return
+
+		} else {
+			fmt.Println("The message has been received!")
+		}
+
+		// Membersihkan dan mencetak pesan
+		message = strings.TrimSpace(message)
+		fmt.Printf("%s: %s\n", conn.RemoteAddr(), message)
+		fmt.Println("The message has been echoed back!")
+
+		// Menyebarkan pesan ke semua client kecuali pengirim
+		broadcastMessage(conn, message)
+	}
+}
+
+func broadcastMessage(sender net.Conn, message string) {
+	mtx.Lock()
+	defer mtx.Unlock()
+
+	for client := range clients {
+		if client != sender {
+			client.Write([]byte(fmt.Sprintf("%s: %s\n", clients[sender], message)))
+		}
+	}
 }
